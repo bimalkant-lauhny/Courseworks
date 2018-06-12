405aac42ad5d07a3be16cb16fd948c765c882dcc
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2018-06-12 10:09:01 +0530

    Classes

---
+++ C++ Fundamentals Including C++ 17/class/Person.h
@@ -0,0 +1,22 @@
+#include <string>
+
+class Person {
+  private:
+    std::string firstname;
+    std::string lastname;
+
+  public:
+    // Explicit constructor
+    Person(std::string firstname, std::string lastname);
+    
+    /* 
+    Ask compiler to generate a default constructor that doesn't take any 
+    arguments since now there is one explicit constructor 
+    */
+    Person()=default;
+
+    // to get the full name of person
+    std::string getName();
+
+    ~Person();
+};
