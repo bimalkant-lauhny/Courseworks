69025959719df3c6d3e9dba773250a15537cdf14
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2018-06-22 21:00:34 +0530

    Template max function

---
+++ C++ Fundamentals Including C++ 17/Templates/Max function/max.cpp
@@ -0,0 +1,45 @@
+// max.cpp : Defines the entry point for the console application.
+//
+
+#include <string>
+using std::string;
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "Person.h"
+#include "BankAccount.h"
+
+
+template <class T> 
+T max(T const& t1, T const& t2)
+{
+	return t1 < t2? t2: t1;
+}
+
+int main()
+{
+	cout << "max of 33 and 44 is " << max(33, 44) << endl;
+	string s1 = "hello";
+	string s2= "world";
+	cout << "max of " << s1 << " and " << s2 << 
+		" is " << max(s1,s2) << endl;
+	Person p1("Kate", "Gregory", 123);
+	Person p2("Someone", "Else", 99);
+	/* 'max' works for Person objects because the operator '<' has been 
+	overloaded.*/
+	cout << "max of " << p1.GetName() << " and " << p2.GetName() << 
+		" is " << max(p1,p2).GetName() << endl;
+	/*
+	Uncomment code below and run.
+	Compiler will produce errors because comparison operator '<' cannot be used
+	with BankAccount objects. Therefore, the operator overload is required for
+	'max' function to work on BankAccount objects.
+	*/
+	//BankAccount b1;
+	//BankAccount b2;
+	//cout << "max of " << b1.GetHolderName() << " and " << b2.GetHolderName() << 
+	//	" is " << max(b1,b2).GetHolderName() << endl;
+	//
+	return 0;
+}
+
